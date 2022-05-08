1040c2067075edc164cf146d2094d96d530cff61
Kevin Najarro <nkevinmanuel94@gmail.com>
2022-05-08 15:50:01 -0600

    Contador de Clics con React Js

---
+++ src/components/Contador.js
@@ -0,0 +1,12 @@
+import react from "react";
+import '../stylesheets/Contador.css'
+
+function Contador({ numClics }){
+  return(
+    <div className="contador">
+      {numClics}
+    </div>
+  );
+}
+
+export default Contador;
